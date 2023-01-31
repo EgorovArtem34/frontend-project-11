0b5210ca95d8aec7de2b287057045a74949f4623
Artem Egorov <artegtor@gmail.com>
2023-01-31 12:47:02 +0400

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,15 +24,18 @@ const config = {
   ],
   module: {
     rules: [
-      // {
-      //   test: /\.(js|jsx)$/i,
-      //   loader: 'babel-loader',
-      // },
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
     ],

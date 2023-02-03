b69bf945337c30a00155396722b83cb088e44848
Artem Egorov <artegtor@gmail.com>
2023-02-03 20:30:07 +0400

    add src/parser.js

---
+++ src/parser.js
@@ -0,0 +1,33 @@
+const getFeed = (doc) => {
+  const title = doc.querySelector('title').textContent;
+  console.log(doc)
+  const description = doc.querySelector('description').textContent;
+  return { title, description };
+};
+const getPosts = (doc) => {
+  const items = doc.querySelectorAll('item');
+  const posts = [];
+  items.forEach((item) => {
+    const post = {
+      title: item.querySelector('title').textContent,
+      description: item.querySelector('description').textContent,
+      link: item.querySelector('link').textContent,
+    };
+    posts.push(post);
+  });
+  return posts;
+}
+
+export default (content) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(content, "application/xml");
+  const err = doc.querySelector('parsererror');
+  if (err) {
+    console.log('error domparser');
+    throw new Error('notContainRSS');
+  }
+  console.log('no error domparser', err, '!!!!!!!!!!', doc);
+  const feed = getFeed(doc);
+  const posts = getPosts(doc);
+  return { feed, posts };
+};

70a6b2aeddb4b155f57feda05571a28de0c21b70
Artem Egorov <artegtor@gmail.com>
2023-02-04 15:34:57 +0400

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
   target: 'web',
+  watchOptions: {
+    poll: 1000, // Check for changes every second
+  },
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),

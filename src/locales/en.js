8bc63e512178c00cd71760ccaedc27718ba1a972
Artem Egorov <artegtor@gmail.com>
2023-02-08 11:34:28 +0400

    update src/locales/

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,10 +1,19 @@
 export default {
   translation: {
     successAdd: 'RSS loaded successfully',
+    feeds: {
+      title: 'Feeds',
+    },
+    posts: {
+      title: 'Posts',
+      button: 'Preview',
+    },
     errors: {
       alreadyExists: 'RSS already exists',
       invalidURL: 'Should be valid URL',
-      notContainRSS: 'need ADD some'
+      notContainRSS: 'need ADD some',
+      networkError: 'Network error',
+      required: 'The field must not be empty',
     },
-  }
-}
+  },
+};
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -12,6 +12,8 @@ export default {
       alreadyExists: 'RSS уже существует',
       invalidURL: 'Ссылка должна быть валидным URL',
       notContainRSS: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
+      required: 'Поле не должно быть пустым',
     },
-  }
-}
+  },
+};
